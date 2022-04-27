b61323cbf5e2aa57312c66d8a25ef4a7678da641
FrancoDiMartino <francogdimartino@gmail.com>
2022-04-27 19:20:39 -0300

    primer commit de la 27620

---
+++ AppCoder/views.py
@@ -0,0 +1,12 @@
+from django import http
+from django.shortcuts import render
+from .models import Curso
+from django.http import HttpResponse
+
+# Create your views here.
+
+def curso(self):
+    curso=Curso(nombre="Curso de Django", comision=12345)
+    curso.save()
+    texto= f"Curso: {curso.nombre} comision: {curso.comision}"
+    return HttpResponse(texto)
