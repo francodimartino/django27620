63c95a63e72042314da344b025a743348ce814d3
FrancoDiMartino <francogdimartino@gmail.com>
2022-05-09 14:59:07 -0300

    final clase 21

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('profesores/', profesores, name='profesores'),
     path('estudiantes/', estudiantes, name='estudiantes'),
     path('entregables/', entregables, name='entregables'),
+   # path('cursosFormulario/', cursosFormulario, name='cursosFormulario'),
+    path('busquedaComision/', busquedaComision, name='busquedaComision'),
+    path('buscar/', buscar, name='buscar'),
     
 
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,6 +2,7 @@ from django import http
 from django.shortcuts import render
 from .models import Curso
 from django.http import HttpResponse
+from AppCoder.forms import CursoFormulario
 
 # Create your views here.
 
@@ -23,8 +24,49 @@ def profesores(request):
 def estudiantes(request):
     return render(request, 'AppCoder/estudiantes.html')
 
-def cursos(request):
-    return render(request, 'AppCoder/cursos.html')
+
 
 def entregables(request):
     return render(request, 'AppCoder/entregables.html')
+
+
+#-----------------------------------------------------------------------------
+def cursos(request):
+
+    if request.method == 'POST':
+
+        miFormulario=CursoFormulario(request.POST)
+
+        if miFormulario.is_valid():
+            informacion=miFormulario.cleaned_data
+            curso=Curso(nombre=informacion['nombre'], comision=informacion['comision'])
+            curso.save()
+            return render(request, 'AppCoder/inicio.html')
+
+    else:
+        miFormulario=CursoFormulario()
+    return render(request, 'AppCoder/cursos.html', {'formulario':miFormulario})
+#-------------------------------------------------------------------
+
+def busquedaComision(request):
+    return render(request, 'AppCoder/busquedaComision.html')
+
+
+def buscar(request):
+    if request.GET['comision']:
+        comision=request.GET['comision']
+        cursos=Curso.objects.filter(comision=comision)
+
+        return render(request, 'AppCoder/resultadosBusqueda.html', {'cursos':cursos, 'comision':comision})
+    else:
+        respuesta="No se ingreso ninguna comision"
+        return render(request, 'AppCoder/resultadosBusqueda.html', {'respuesta':respuesta})
+
+
+
+
+
+        
+        
+
+    return render(request, 'AppCoder/cursosFormulario.html')
